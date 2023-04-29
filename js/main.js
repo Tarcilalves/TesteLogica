dca1a8b326f7564cb1ed71e815c215c085defd73
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-04-28 23:34:16 -0300

    função cumprimentar pessoa com interação do usuario

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 
-
-
-
-
-
-
-
 function cumprimentar(){
     document.getElementById('resultado').innerHTML = 'Oi, gente!';
     console.log('Oi gente!')
    }
    
    cumprimentar();
+
+
+   function cumprimentaPessoa(){
+    var nome = document.getElementById("nome").value;
+    var mensagem = document.getElementById("mensagem");    
+    document.getElementById('saudacao').innerHTML = `${cumprimentar()} Meu nome é ${nome}`
+  }
+  
+  function cumprimentar(){
+    return 'Oi gente!'
+   }
+   
+ 
+   
+ 
